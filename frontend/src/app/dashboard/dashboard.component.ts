e92713aa83590e9c820df9272f0661faecbf5a7c
Emil Sroka <emil.sroka@stazysta.comarch.pl>
2020-07-07 09:49:32 +0200

    feat: add support for api on frontend

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProvincesService } from './provinces/provinces.service';
 import { CasesService } from './cases/cases.service';
+import { DailyCases } from './types/data.types';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,9 +10,15 @@ import { CasesService } from './cases/cases.service';
 })
 export class DashboardComponent {
   public title = 'Coronavirus cases in Poland';
+  public date = new Date(2020, 5, 20);
+  public cases = [];
 
   constructor(
     public provincesService: ProvincesService,
     public casesService: CasesService
-  ) {}
+  ) {
+    casesService
+      .getCases(this.date)
+      .subscribe(({ cases }: DailyCases) => (this.cases = cases));
+  }
 }
--- frontend/src/app/dashboard/map/map.helpers.ts
+++ frontend/src/app/dashboard/map/map.helpers.ts
@@ -1,19 +1,14 @@
-import { ProvinceCases } from './map.types';
+import { Cases } from '../types/data.types';
 
-export function max(currentMax: number, { cases }: ProvinceCases): number {
+export function max(currentMax: number, { cases }: Cases): number {
   return currentMax < cases ? cases : currentMax;
 }
 
-export function byID(targetID): (ProvinceCases) => boolean {
-  return ({ id }: ProvinceCases) => id === targetID;
+export function byID(targetID): (Cases) => boolean {
+  return ({ id }: Cases) => id === targetID;
 }
 
 export function calculateLightness(value: number, maxValue: number): number {
   const normalizedValue = 1 - value / maxValue;
-  const exponentialValue = Math.exp(normalizedValue);
-  return 50 + (50 * exponentialValue) / Math.exp(1);
-}
-
-export function getFirstValue(array: ProvinceCases[]): [number, number] {
-  return [array[0].cases, array[0].cases];
+  return 50 + 50 * normalizedValue;
 }
