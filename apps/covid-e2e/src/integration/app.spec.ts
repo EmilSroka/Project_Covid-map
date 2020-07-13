671490b21786c295455597a1cd7567208ad3b22b
Emil Sroka <emil.sroka@stazysta.comarch.pl>
2020-07-13 12:56:31 +0200

    test: add e2e tests

--- apps/covid-e2e/src/integration/app.spec.ts
+++ apps/covid-e2e/src/integration/app.spec.ts
@@ -1,13 +1,37 @@
-import { getGreeting } from '../support/app.po';
+import { verify } from 'cypress/types/sinon';
 
 describe('covid', () => {
-  beforeEach(() => cy.visit('/'));
+  const pandemicStart = '2020-03-04';
+  const backednURL = 'http://localhost:7000/api/day/*';
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  beforeEach(() => {
+    cy.server()
+      .route({
+        force404: true,
+        url: backednURL,
+      })
+      .as('getData');
+    cy.visit('/');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to covid!');
+  it('should display title', () => {
+    cy.contains('Coronavirus cases in Poland on');
+  });
+
+  it('should display map', () => {
+    cy.get('.map').should('exist');
+    cy.get('.map').find('path');
+  });
+
+  it('should display error message on request failure', () => {
+    cy.wait('@getData'); //.its('status').should('be', 404);
+    cy.contains('No response from backend');
+  });
+
+  it('should display date picker', () => {
+    cy.get('.date-picker').should('exist');
+    cy.get('.date-picker').should('have.value', pandemicStart);
+    cy.get('.date-picker').type('2020-05-01');
+    cy.get('.date-picker').should('have.value', '2020-05-01');
   });
 });
